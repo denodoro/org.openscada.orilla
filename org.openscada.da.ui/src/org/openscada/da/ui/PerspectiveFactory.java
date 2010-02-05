8734bd11ea86fd351cdde82dfe2bb25390e6af81
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-05 10:06:14 +0000

    fix layout
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/orilla/trunk@6805 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.ui/src/org/openscada/da/ui/PerspectiveFactory.java
+++ org.openscada.da.ui/src/org/openscada/da/ui/PerspectiveFactory.java
@@ -1,5 +1,6 @@
 package org.openscada.da.ui;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -9,6 +10,16 @@ public class PerspectiveFactory implements IPerspectiveFactory
     public void createInitialLayout ( final IPageLayout layout )
     {
         layout.setEditorAreaVisible ( false );
+
+        final IFolderLayout folder = layout.createFolder ( "org.openscada.da.ui.mainFolder", IPageLayout.RIGHT, 1.0f, IPageLayout.ID_EDITOR_AREA );
+
+        folder.addPlaceholder ( "*" );
+        folder.addPlaceholder ( "*:*" );
+        folder.addView ( "org.openscada.da.test.views.RealTimeList" );
+        folder.addPlaceholder ( "org.openscada.da.test.views.RealTimeList:*" );
+
+        layout.addView ( "org.openscada.core.ui.connection.ConnectionView", IPageLayout.LEFT, 0.25f, "org.openscada.da.ui.mainFolder" );
+        layout.addView ( "org.eclipse.pde.runtime.LogView", IPageLayout.BOTTOM, 0.75f, "org.openscada.da.ui.mainFolder" );
     }
 
 }
