51024ec515d1dc99e843343e019841bae32a0fe4
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-10 16:16:51 +0000

    initial checkin
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/orilla/trunk@6923 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.ca.ui/src/org/openscada/ca/ui/perspectives/PerspectiveFactory.java
@@ -0,0 +1,12 @@
+package org.openscada.ca.ui.perspectives;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveFactory implements IPerspectiveFactory
+{
+    public void createInitialLayout ( final IPageLayout layout )
+    {
+    }
+
+}
