c1eaf22d4a4a713ccfdb8124dcf270cd92bbf687
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-11 16:40:16 +0000

    add license stuff
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/orilla/trunk@6955 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ui.utils/src/org/openscada/ui/utils/toggle/ToggleError.java
+++ org.openscada.ui.utils/src/org/openscada/ui/utils/toggle/ToggleError.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package org.openscada.ui.utils.toggle;
 
 public class ToggleError extends RuntimeException
@@ -9,17 +28,17 @@ public class ToggleError extends RuntimeException
         super ();
     }
 
-    public ToggleError ( String message, Throwable cause )
+    public ToggleError ( final String message, final Throwable cause )
     {
         super ( message, cause );
     }
 
-    public ToggleError ( String message )
+    public ToggleError ( final String message )
     {
         super ( message );
     }
 
-    public ToggleError ( Throwable cause )
+    public ToggleError ( final Throwable cause )
     {
         super ( cause );
     }
