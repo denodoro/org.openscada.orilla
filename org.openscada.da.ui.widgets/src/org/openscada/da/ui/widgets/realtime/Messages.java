07b7ba9c1b4ee198b189608091bedd3b7f14bde6
Jens Reimann <jens.reimann@th4-systems.com>
2010-03-08 10:01:36 +0000

    fix package name
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/orilla/trunk@7322 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.ui.widgets/src/org/openscada/da/ui/widgets/realtime/Messages.java
+++ org.openscada.da.ui.widgets/src/org/openscada/da/ui/widgets/realtime/Messages.java
@@ -24,7 +24,7 @@ import java.util.ResourceBundle;
 
 public class Messages
 {
-    private static final String BUNDLE_NAME = "org.openscada.da.client.base.realtime.messages"; //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "org.openscada.da.ui.widgets.realtime"; //$NON-NLS-1$
 
     private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle ( BUNDLE_NAME );
 
