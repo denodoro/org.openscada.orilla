0de8a6f42ccb025d3c310f38902222afc6590e07
Jens Reimann <jens.reimann@th4-systems.com>
2010-05-20 14:19:42 +0000

    switch to LGPLv3
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/orilla/trunk@7994 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.da.client.chart/src/org/openscada/da/client/chart/Activator.java
+++ org.openscada.da.client.chart/src/org/openscada/da/client/chart/Activator.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
+ *
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.da.client.chart;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -27,7 +46,7 @@ public class Activator extends AbstractUIPlugin
      * (non-Javadoc)
      * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
      */
-    public void start ( BundleContext context ) throws Exception
+    public void start ( final BundleContext context ) throws Exception
     {
         super.start ( context );
     }
@@ -36,7 +55,7 @@ public class Activator extends AbstractUIPlugin
      * (non-Javadoc)
      * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
      */
-    public void stop ( BundleContext context ) throws Exception
+    public void stop ( final BundleContext context ) throws Exception
     {
         plugin = null;
         super.stop ( context );
