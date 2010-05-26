4201bd420298065172a4fa55b10c1d96eeb78f41
Jens Reimann <jens.reimann@th4-systems.com>
2010-05-26 09:12:45 +0000

    externalize strings
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/orilla/trunk@8074 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ae.ui/src/org/openscada/ae/ui/Activator.java
+++ org.openscada.ae.ui/src/org/openscada/ae/ui/Activator.java
@@ -29,7 +29,7 @@ public class Activator extends AbstractUIPlugin
 {
 
     // The plug-in ID
-    public static final String PLUGIN_ID = "org.openscada.ae.ui";
+    public static final String PLUGIN_ID = "org.openscada.ae.ui"; //$NON-NLS-1$
 
     // The shared instance
     private static Activator plugin;
