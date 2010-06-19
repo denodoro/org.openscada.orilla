429343eae51285041313f5e3bae7b5cc79845566
Jens Reimann <jens.reimann@th4-systems.com>
2010-06-19 17:33:27 +0000

    cleanup!!
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/orilla/trunk@8303 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ui.localization/src/org/openscada/ui/localization/timezone/TimeZonePreferencePage.java
+++ org.openscada.ui.localization/src/org/openscada/ui/localization/timezone/TimeZonePreferencePage.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
+ *
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.ui.localization.timezone;
 
 import java.util.Arrays;
@@ -24,20 +43,20 @@ public class TimeZonePreferencePage extends FieldEditorPreferencePage implements
     protected void createFieldEditors ()
     {
         final List<String> tzs = Arrays.asList ( TimeZone.getAvailableIDs () );
-        Collections.sort ( tzs);
+        Collections.sort ( tzs );
         final String[][] entries = new String[tzs.size ()][2];
         int i = 0;
-        for ( String id : tzs )
+        for ( final String id : tzs )
         {
             entries[i][0] = id;
             entries[i][1] = id;
             i += 1;
         }
-        FieldEditor field = new ComboFieldEditor ( "timeZone", "Time Zone", entries, getFieldEditorParent() );
+        final FieldEditor field = new ComboFieldEditor ( "timeZone", "Time Zone", entries, getFieldEditorParent () );
         addField ( field );
     }
 
-    public void init ( IWorkbench workbench )
+    public void init ( final IWorkbench workbench )
     {
         setPreferenceStore ( Activator.getDefault ().getPreferenceStore () );
         setDescription ( "Set Timezone used within Application" );
