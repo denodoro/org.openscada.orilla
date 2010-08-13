3962e37ed216c4db48ed30153dc8078f594b9055
Jens Reimann <jens.reimann@th4-systems.com>
2010-08-13 12:05:08 +0000

    initial import
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/orilla/trunk@8357 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.ca.ui.exporter/src/org/openscada/ca/ui/exporter/wizard/ExportWizard.java
@@ -0,0 +1,135 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
+ *
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+package org.openscada.ca.ui.exporter.wizard;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.concurrent.ExecutionException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IExportWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.statushandlers.StatusManager;
+import org.openscada.ca.FactoryInformation;
+import org.openscada.ca.connection.provider.ConnectionService;
+import org.openscada.ca.ui.exporter.Activator;
+import org.openscada.ca.ui.util.ConfigurationHelper;
+import org.openscada.ca.ui.util.OscarWriter;
+import org.openscada.ui.databinding.AdapterHelper;
+
+public class ExportWizard extends Wizard implements IExportWizard
+{
+
+    private ConnectionService connection;
+
+    private FileNamePage page;
+
+    public ExportWizard ()
+    {
+        setNeedsProgressMonitor ( true );
+        setWindowTitle ( "Export" );
+
+        IDialogSettings settings = Activator.getDefault ().getDialogSettings ().getSection ( "exportWizard" ); //$NON-NLS-1$
+        if ( settings == null )
+        {
+            settings = Activator.getDefault ().getDialogSettings ().addNewSection ( "exportWizard" ); //$NON-NLS-1$
+        }
+
+        setDialogSettings ( Activator.getDefault ().getDialogSettings ().getSection ( "exportWizard" ) ); //$NON-NLS-1$
+
+    }
+
+    @Override
+    public void addPages ()
+    {
+        super.addPages ();
+        addPage ( this.page = new FileNamePage () );
+    }
+
+    @Override
+    public boolean performFinish ()
+    {
+        try
+        {
+            getContainer ().run ( true, true, new IRunnableWithProgress () {
+
+                public void run ( final IProgressMonitor monitor ) throws InvocationTargetException, InterruptedException
+                {
+                    try
+                    {
+                        handleFinish ( monitor );
+                    }
+                    catch ( final Exception e )
+                    {
+                        throw new InvocationTargetException ( e );
+                    }
+                }
+            } );
+            return true;
+        }
+        catch ( final Exception e )
+        {
+            final Status status = new Status ( IStatus.ERROR, Activator.PLUGIN_ID, "Failed to export data", e );
+            StatusManager.getManager ().handle ( status, StatusManager.BLOCK );
+            return false;
+        }
+    }
+
+    protected void handleFinish ( final IProgressMonitor monitor ) throws InterruptedException, ExecutionException, FileNotFoundException, IOException
+    {
+        // load data
+        final Collection<FactoryInformation> data = ConfigurationHelper.loadData ( monitor, this.connection.getConnection () );
+
+        // write to OSCAR
+        final OscarWriter writer = new OscarWriter ( ConfigurationHelper.convert ( data ), null );
+        writer.write ( this.page.getFile () );
+    }
+
+    public void init ( final IWorkbench workbench, final IStructuredSelection selection )
+    {
+        this.connection = getConnection ( selection );
+    }
+
+    private ConnectionService getConnection ( final IStructuredSelection selection )
+    {
+        if ( selection == null )
+        {
+            return null;
+        }
+
+        final Object o = AdapterHelper.adapt ( selection.getFirstElement (), org.openscada.core.connection.provider.ConnectionService.class );
+        if ( o instanceof ConnectionService )
+        {
+            return (ConnectionService)o;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+}
---
+++ org.openscada.ca.ui.exporter/src/org/openscada/ca/ui/exporter/wizard/FileNamePage.java
@@ -0,0 +1,149 @@
+/*
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
+ *
+ * OpenSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * OpenSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+package org.openscada.ca.ui.exporter.wizard;
+
+import java.io.File;
+
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+public class FileNamePage extends WizardPage
+{
+
+    private Text fileName;
+
+    public FileNamePage ()
+    {
+        super ( "fileNamePage" );
+    }
+
+    public void createControl ( final Composite parent )
+    {
+        final Composite wrapper = new Composite ( parent, SWT.NONE );
+        wrapper.setLayout ( new GridLayout ( 3, false ) );
+
+        Label label;
+
+        label = new Label ( wrapper, SWT.NONE );
+        label.setText ( "File:" );
+
+        this.fileName = new Text ( wrapper, SWT.BORDER );
+        this.fileName.setLayoutData ( new GridData ( SWT.FILL, SWT.CENTER, true, false ) );
+        final String file = getWizard ().getDialogSettings ().get ( "fileNamePage.file" );
+        if ( file != null )
+        {
+            this.fileName.setText ( file );
+        }
+        this.fileName.addModifyListener ( new ModifyListener () {
+
+            public void modifyText ( final ModifyEvent e )
+            {
+                update ();
+            }
+        } );
+
+        final Button selectFile = new Button ( wrapper, SWT.PUSH );
+        selectFile.setText ( "Browse..." );
+        selectFile.addSelectionListener ( new SelectionAdapter () {
+            @Override
+            public void widgetSelected ( final SelectionEvent e )
+            {
+                handleSelectFile ();
+            }
+        } );
+
+        setControl ( wrapper );
+
+        update ();
+    }
+
+    protected void update ()
+    {
+        try
+        {
+            validate ();
+            setMessage ( null, IMessageProvider.NONE );
+            setPageComplete ( true );
+        }
+        catch ( final Exception e )
+        {
+            setMessage ( e.getLocalizedMessage (), IMessageProvider.ERROR );
+            setPageComplete ( false );
+        }
+    }
+
+    private void validate () throws Exception
+    {
+        final String fileName = this.fileName.getText ();
+
+        if ( fileName.length () == 0 )
+        {
+            throw new IllegalStateException ( "No file selected" );
+        }
+
+        final File file = new File ( fileName );
+
+        if ( file.exists () && !file.isFile () )
+        {
+            throw new IllegalArgumentException ( String.format ( "File %s exists but is not a normal file", fileName ) );
+        }
+        if ( !file.canWrite () && file.exists () )
+        {
+            throw new IllegalArgumentException ( String.format ( "Cannot write to file %s", fileName ) );
+        }
+    }
+
+    protected void handleSelectFile ()
+    {
+        final FileDialog dlg = new FileDialog ( getShell (), SWT.SAVE );
+        dlg.setFilterExtensions ( new String[] { "*.oscar", "*.*" } ); //$NON-NLS-1$ //$NON-NLS-2$ 
+        dlg.setFilterNames ( new String[] { "OSCAR File", "All types" } );
+
+        if ( this.fileName.getText ().length () > 0 )
+        {
+            dlg.setFileName ( this.fileName.getText () );
+        }
+        dlg.setFilterIndex ( 0 );
+
+        final String file = dlg.open ();
+        if ( file != null )
+        {
+            this.fileName.setText ( file );
+            getWizard ().getDialogSettings ().put ( "fileNamePage.file", file ); //$NON-NLS-1$
+        }
+    }
+
+    public File getFile ()
+    {
+        return new File ( this.fileName.getText () );
+    }
+
+}
