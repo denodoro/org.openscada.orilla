a134348bc621f3d20de6e9ab21a1062d9a35caae
Jens Reimann <jens.reimann@th4-systems.com>
2011-05-27 18:16:21 +0200

    fix possible NPE

--- org.openscada.ui.databinding/src/org/openscada/ui/databinding/AbstractSelectionHandler.java
+++ org.openscada.ui.databinding/src/org/openscada/ui/databinding/AbstractSelectionHandler.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2010 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -33,7 +33,8 @@ public abstract class AbstractSelectionHandler extends AbstractHandler
 
     protected IWorkbenchPage getActivePage ()
     {
-        return this.activeWindow.getActivePage ();
+        final IWorkbenchWindow window = getWorkbenchWindow ();
+        return window.getActivePage ();
     }
 
     /**
