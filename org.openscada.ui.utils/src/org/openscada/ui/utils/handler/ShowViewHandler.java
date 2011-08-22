7ace74a270d66211154237ff29a8d2480a701f88
Jens Reimann <jens.reimann@th4-systems.com>
2011-08-22 11:40:25 +0200

    add license header
    Change-Id: I04bfb29b7eac3b3c7920b65b9006c6fa043c8736

--- org.openscada.ui.utils/src/org/openscada/ui/utils/handler/ShowViewHandler.java
+++ org.openscada.ui.utils/src/org/openscada/ui/utils/handler/ShowViewHandler.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the openSCADA project
+ * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ *
+ * openSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * openSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with openSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.ui.utils.handler;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -22,15 +41,16 @@ public class ShowViewHandler extends AbstractHandler
     {
     }
 
+    @Override
     public final Object execute ( final ExecutionEvent event ) throws ExecutionException
     {
-        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked ( event );
+        final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked ( event );
 
         final Object value = event.getParameter ( PARAMETER_NAME_VIEW_ID );
 
         try
         {
-            String[] viewIds = ( (String)value ).split ( ":" );
+            final String[] viewIds = ( (String)value ).split ( ":" );
             if ( viewIds.length == 1 )
             {
                 openView ( viewIds[0], null, window );
@@ -40,7 +60,7 @@ public class ShowViewHandler extends AbstractHandler
                 openView ( viewIds[0], viewIds[1], window );
             }
         }
-        catch ( PartInitException e )
+        catch ( final PartInitException e )
         {
             throw new ExecutionException ( "Part could not be initialized", e ); //$NON-NLS-1$
         }
