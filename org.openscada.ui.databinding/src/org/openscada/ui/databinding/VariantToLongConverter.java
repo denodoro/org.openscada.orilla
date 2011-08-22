644ba6dd0841fed55455f6e61ebda96a0cf2bbf0
Jens Reimann <jens.reimann@th4-systems.com>
2011-08-22 13:55:37 +0200

    moved to orilla
    Change-Id: Ic0efcf87ff84a6191c1bbf52e0c3af5600bdecaa

---
+++ org.openscada.ui.databinding/src/org/openscada/ui/databinding/VariantToLongConverter.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the openSCADA project
+ * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ *
+ * openSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * openSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with openSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
+package org.openscada.ui.databinding;
+
+import org.eclipse.core.databinding.conversion.IConverter;
+import org.openscada.core.Variant;
+
+public class VariantToLongConverter implements IConverter
+{
+    private final Long defaultValue;
+
+    public VariantToLongConverter ()
+    {
+        this ( null );
+    }
+
+    public VariantToLongConverter ( final Long defaultValue )
+    {
+        this.defaultValue = defaultValue;
+    }
+
+    @Override
+    public Object getFromType ()
+    {
+        return Variant.class;
+    }
+
+    @Override
+    public Object getToType ()
+    {
+        return Long.class;
+    }
+
+    @Override
+    public Object convert ( final Object fromObject )
+    {
+        return ( (Variant)fromObject ).asLong ( this.defaultValue );
+    }
+}
