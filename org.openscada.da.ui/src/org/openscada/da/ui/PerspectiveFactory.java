9027590a634418fde44607425743761267d78f8b
Jens Reimann <jens.reimann@th4-systems.com>
2012-07-13 09:29:17 +0200

    implement some fixes for E4

--- org.openscada.da.ui/src/org/openscada/da/ui/PerspectiveFactory.java
+++ org.openscada.da.ui/src/org/openscada/da/ui/PerspectiveFactory.java
@@ -19,7 +19,6 @@
 
 package org.openscada.da.ui;
 
-import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -29,16 +28,6 @@ public class PerspectiveFactory implements IPerspectiveFactory
     public void createInitialLayout ( final IPageLayout layout )
     {
         layout.setEditorAreaVisible ( false );
-
-        final IFolderLayout folder = layout.createFolder ( "org.openscada.da.ui.mainFolder", IPageLayout.RIGHT, 1.0f, IPageLayout.ID_EDITOR_AREA );
-
-        folder.addPlaceholder ( "*" );
-        folder.addPlaceholder ( "*:*" );
-        folder.addView ( "org.openscada.da.test.views.RealTimeList" );
-        folder.addPlaceholder ( "org.openscada.da.test.views.RealTimeList:*" );
-
-        layout.addView ( "org.openscada.core.ui.connection.ConnectionView", IPageLayout.LEFT, 0.25f, "org.openscada.da.ui.mainFolder" );
-        layout.addPlaceholder ( "org.eclipse.pde.runtime.LogView", IPageLayout.BOTTOM, 0.75f, "org.openscada.da.ui.mainFolder" );
     }
 
 }
