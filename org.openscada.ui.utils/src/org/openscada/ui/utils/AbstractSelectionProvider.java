b925ddf7f63ec0944b405267d94f34d2c9978fee
Jens Reimann <jens.reimann@th4-systems.com>
2012-10-17 16:31:46 +0200

    init with empty selection

--- org.openscada.ui.utils/src/org/openscada/ui/utils/AbstractSelectionProvider.java
+++ org.openscada.ui.utils/src/org/openscada/ui/utils/AbstractSelectionProvider.java
@@ -26,12 +26,13 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
 
 public class AbstractSelectionProvider implements ISelectionProvider
 {
     private final Set<ISelectionChangedListener> listeners = new LinkedHashSet<ISelectionChangedListener> ();
 
-    private ISelection selection;
+    private ISelection selection = StructuredSelection.EMPTY;
 
     @Override
     public void setSelection ( final ISelection selection )
