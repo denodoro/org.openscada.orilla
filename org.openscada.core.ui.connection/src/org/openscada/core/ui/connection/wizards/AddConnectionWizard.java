95686c8574e5a223e84ab740d754805632131557
Jens Reimann <jens.reimann@th4-systems.com>
2012-11-28 16:44:15 +0100

    add "edit" dialog for connection entries

--- org.openscada.core.ui.connection/src/org/openscada/core/ui/connection/wizards/AddConnectionWizard.java
+++ org.openscada.core.ui.connection/src/org/openscada/core/ui/connection/wizards/AddConnectionWizard.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2010 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -43,6 +43,11 @@ public class AddConnectionWizard extends Wizard implements INewWizard
 
     private ConnectionDescriptor preset;
 
+    protected boolean isUpdateMode ()
+    {
+        return false;
+    }
+
     @Override
     public boolean performFinish ()
     {
@@ -52,7 +57,14 @@ public class AddConnectionWizard extends Wizard implements INewWizard
         {
             if ( connectionInformation != null )
             {
-                this.store.add ( connectionInformation );
+                if ( isUpdateMode () )
+                {
+                    this.store.update ( this.preset, connectionInformation );
+                }
+                else
+                {
+                    this.store.add ( connectionInformation );
+                }
             }
         }
         catch ( final CoreException e )
