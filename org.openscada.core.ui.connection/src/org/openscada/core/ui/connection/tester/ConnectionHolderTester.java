644d54d0c2c89ab1410dbdc77c6a9eab543f598f
Jens Reimann <jens.reimann@th4-systems.com>
2012-11-28 16:34:22 +0100

    fix possible NPE

--- org.openscada.core.ui.connection/src/org/openscada/core/ui/connection/tester/ConnectionHolderTester.java
+++ org.openscada.core.ui.connection/src/org/openscada/core/ui/connection/tester/ConnectionHolderTester.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2010 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -60,6 +60,18 @@ public class ConnectionHolderTester extends PropertyTester
 
         if ( "interfaceName".equals ( property ) && expectedValue != null ) //$NON-NLS-1$
         {
+            if ( holder.getConnectionInformation () == null )
+            {
+                return false;
+            }
+            if ( holder.getConnectionInformation ().getConnectionInformation () == null )
+            {
+                return false;
+            }
+            if ( holder.getConnectionInformation ().getConnectionInformation ().getInterface () == null )
+            {
+                return false;
+            }
             return holder.getConnectionInformation ().getConnectionInformation ().getInterface ().equals ( expectedValue );
         }
 
